5f077bdec1fd5062decc44d4706450abae2b6939
pnusw14 <dustofjade@gmail.com>
2024-02-24 15:11:49 +0900

    privateRoute

--- src/App.js
+++ src/App.js
@@ -11,20 +11,40 @@ import Rounge from "./components/board/Rounge/Rounge";
 import Board from "./components/board/Board";
 import Find from "./components/find/Find";
 import Test from "./components/Home/Test";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Main/>}/>
+      <Route path="/user/login" element={<Login/>}/>
+            <Route path="/test" element={<Test/>}/>
+            <Route path="/find" element={<Find/>}/>
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
 
   return (
       <BrowserRouter>
       <RecoilRoot>
         <Navbar/>
+ 
         <div className=""></div>
         <Routes>
-            <Route path="/" element={<Main/>}/>
             <Route path="/home" element={<Home/>}/>
-            <Route path="/find" element={<Find/>}/>
-            <Route path="/user/login" element={<Login/>}/>
-            <Route path="/test" element={<Test/>}/>
+            
+          
             <Route path="/user/findpwd" element={<FindPwd/>}/>
             <Route path="/user/join" element={<Join/>}/>
             <Route path="/user/mypage" element={<MyPage/>}/>
@@ -37,3 +57,5 @@ function App() {
 }
 
 export default App;
+
+
